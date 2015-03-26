63657cb837bcb274c93f7a20e2d6f5cfa733af20
alfonsogacevedo <alfonsogacevedo@gmail.com>
2015-03-25 18:33:11 -0700

    Added POST requests

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from crawler import get_event_urls
 
 app = Flask(__name__)
 
-@app.route('/api/v1.0/crawl', methods=['GET'])
+@app.route('/api/v1.0/crawl', methods=['GET','POST'])
 def get_tasks():
-    search_url = request.args.get('url', '')
+    search_url = request.args.get('url', '') or request.form.get('url','')
     urls = get_event_urls(search_url)
     return jsonify({'urls': urls})
 
